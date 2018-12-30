88b7d517f938d4e7aa3afb4b21f2b63498dbee9e
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-12-30 23:48:05 +0530

    Refactored the encoders and decoders.

--- main.py
+++ main.py
@@ -1,100 +1,101 @@
+char_to_trinary = {
+    " ": "000000",
+    "a": "000012",
+    "b": "002111",
+    "c": "002121",
+    "d": "000211",
+    "e": "000001",
+    "f": "001121",
+    "g": "000221",
+    "h": "001111",
+    "i": "000011",
+    "j": "001222",
+    "k": "000212",
+    "l": "001211",
+    "m": "000022",
+    "n": "000021",
+    "o": "000222",
+    "p": "001221",
+    "q": "002212",
+    "r": "000121",
+    "s": "000111",
+    "t": "000002",
+    "u": "000112",
+    "v": "001112",
+    "w": "000122",
+    "x": "002112",
+    "y": "002122",
+    "z": "002211",
+    "": "",
+    "0": "022222",
+    "1": "012222",
+    "2": "011222",
+    "3": "011122",
+    "4": "011112",
+    "5": "011111",
+    "6": "021111",
+    "7": "022111",
+    "8": "022211",
+    "9": "022221",
+    "&": "012111",
+    "'": "122221",
+    "@": "122121",
+    ")": "212212",
+    "(": "021221",
+    ":": "222111",
+    ",": "221122",
+    "=": "021112",
+    "!": "212122",
+    ".": "121212",
+    "-": "211112",
+    "+": "012121",
+    '"': "121121",
+    "?": "112211",
+    "/": "021121",
+}
+
+trinary_to_char = dict((v, k) for k, v in char_to_trinary.items())
+
+# print(trinary_to_char)
+
 def trinary_encoder(inputs):
-    string_lower=str(inputs).lower()
-    output_string=''
-    dictionary={' ': '0', 'a': '12', 'b': '2111', 'c': '2121', 'd': '211', 'e': '1', 'f': '1121',
-            'g': '221', 'h': '1111', 'i': '11', 'j': '1222', 'k': '212', 'l': '1211', 'm': '22',
-            'n': '21', 'o': '222', 'p': '1221', 'q': '2212', 'r': '121', 's': '111', 't': '2',
-            'u': '112', 'v': '1112', 'w': '122', 'x': '2112', 'y': '2122', 'z': '2211', '': '', 
-            '0': '22222', '1': '12222', '2': '11222', '3': '11122', '4': '11112', '5': '11111', 
-            '6': '21111', '7': '22111', '8': '22211', '9': '22221', '&': '12111', "'": '122221', 
-            '@': '122121', ')': '212212', '(': '21221', ':': '222111', ',': '221122', '=': '21112',
-            '!': '212122', '.': '121212', '-': '211112', '+': '12121', '"': '121121', '?': '112211',
-            '/': '21121'}
-    for s in string_lower:
-        output_string=output_string+str(dictionary[s])+'0'
-    output_string=output_string[:-1]
-    return output_string
+    return "".join([char_to_trinary[i] for i in list(str(inputs).lower())])
 
-def trinary_decoder(inputs):
-    string_input=str(inputs)
-    string_output=''
-    tmp=''
-    inv_dictionary={'0': ' ', '12': 'a', '2111': 'b', '2121': 'c', '211': 'd', '1': 'e', '1121': 'f', '221': 'g', '1111': 'h',
-                    '11': 'i',         '1222': 'j',          '212': 'k', '1211': 'l', '22': 'm', '21': 'n', '222': 'o',
-                    '1221': 'p', '2212': 'q', '121': 'r', '111': 's',  '2': 't', '112': 'u', '1112': 'v', '122': 'w',
-                    '2112': 'x', '2122': 'y', '2211': 'z', '': '', '22222': '0', '12222': '1',        '11222': '2',
-                    '11122': '3',              '11112': '4', '11111': '5', '21111': '6', '22111': '7', '22211': '8',
-                    '22221': '9',   '12111': '&', '122221': "'", '122121': '@',  '212212': ')', 
-                    '21221': '(', '222111': ':', '221122': ',', '21112': '=',          '212122': '!', 
-                    '121212': '.', '211112': '-', '12121':               '+', '121121': '"', '112211': '?',
-                    '21121': '/'}
-    for i in range(len(string_input)):
-        
-        if string_input[i]!='0':
-            tmp += string_input[i]
-            
-        else:
-            
-            string_output +=inv_dictionary[tmp]
-            tmp=''
-            if string_input[i+1:i+3]=='00':
-                if string_input[i+3]!='0':
-                    string_output+=' '
-                else:
-                    None
-            else:
-                None
-    string_output+=inv_dictionary[tmp]
-    return string_output
+# print(trinary_encoder("BooomBOOOOM"))
 
+def trinary_decoder(inp):
+    chars = [str(inp)[i : i + 6] for i in range(0, len(inp), 6)]
+    return "".join([trinary_to_char[i] for i in chars])
 
-def trinary2morse(inputs,separator=' / '):
-    input_string=str(inputs)
-    separator_string=str(separator)
-    output_string=''
-    for i in range(len(input_string)):
-        if input_string[i]=='1':
-            output_string+='.'
-        elif input_string[i]=='2':
-            output_string+='-'
-        elif input_string[i]=='0':
-            if input_string[i+1:i+3]=='00':
-                if input_string[i+3]!='0':
-                    output_string+=separator_string
-                else:
-                    None
-            else:
-                output_string+=' '
-        else:
-            None
-    return output_string
+# print(trinary_decoder("002111000222000222000222000022002111000222000222000222000222000022"))
 
+separator = '/'
 
-def morse2trinary(inputs,separator='/'):
-    input_string=str(inputs)
-    separator_string=str(separator)
-    output_string=''
-    for i in range(len(input_string)):
-        if input_string[i]=='.':
-            output_string+='1'
-        elif input_string[i]=='-':
-            output_string+='2'
-        elif input_string[i]==separator_string:
-            output_string+='0'
-        elif input_string[i]==' ':
-            if input_string[i+1]!=' ':
-                output_string+='0'
-            else:
-                None
-        else:
-            None
-    return output_string
+trinary_to_morse = {'1': '.', '2': '-', '0': separator}
+morse_to_trinary = dict((v, k) for k, v in trinary_to_morse.items())
 
+def trinary2morse(inp):
 
+    chars = [int(str(inp)[i : i + 6]) for i in range(0, len(inp), 6)]   # strip down all zeros in the beginning
+    morse = [ ''.join([trinary_to_morse[i] for i in str(char)]) for char in chars]
 
+    return ' '.join(morse) # joining with ' ' provides the space between two chars
+
+# print(trinary2morse(trinary_encoder('SOS Notice Me Senpai.')))
+
+def morse2trinary(inp):
+
+    chars = inp.split() # splits stuff with space
+    trinary = ["".join([morse_to_trinary[i] for i in str(char)]) for char in chars]
+    trinary = ['{0:06d}'.format(int(n)) for n in trinary] # add the leading zeroes back again.
+
+    return ''.join(trinary)
+
+# print(trinary_decoder(morse2trinary('... --- ... / -. --- - .. -.-. . / -- . / ... . -. .--. .- .. .-.-.-')))
 
 def main_function():
-    print("""Welcome to the Trinary morse endcoder.
+    print(
+        """Welcome to the Trinary morse endcoder.
             Make sure to enter everything in string format.
             Chose an option first.
             1. Text to trinary : 'a'
@@ -102,34 +103,35 @@ def main_function():
             3. Trinary to morse: 'c'
             4. Morse to trinary: 'd'
             5. Text to morse   : 'e'
-            6. Morse to text   : 'f' """)
-    function=str(input("Enter what you want to do: "))
-    function=function.lower()
-    user_input=str(input("Enter the text: "))
-    if function=='a':
+            6. Morse to text   : 'f' """
+    )
+    function = str(input("Enter what you want to do: "))
+    function = function.lower()
+    user_input = str(input("Enter the text: "))
+    if function == "a":
         return trinary_encoder(user_input)
-    elif function=='b':
+    elif function == "b":
         return trinary_decoder(user_input)
-    elif function=='c':
+    elif function == "c":
         return trinary2morse(user_input)
-    elif function=='d':
+    elif function == "d":
         return morse2trinary(user_input)
-    elif function=='e':
+    elif function == "e":
         return trinary2morse(trinary_encoder(user_input))
-    elif function=='f':
+    elif function == "f":
         return trinary_decoder(morse2trinary(user_input))
     else:
         return None
 
 
 while True:
-    output=main_function()
+    output = main_function()
     print(output)
     if output:
-        confirmation=str(input("Do you want another round? y/n: "))
-        if confirmation in ['N','n','no', 'No', 'NO']:
+        confirmation = str(input("Do you want another round? y/n: "))
+        if confirmation in ["N", "n", "no", "No", "NO"]:
             break
-        elif confirmation in ['y','Y','yes','Yes']:
+        elif confirmation in ["y", "Y", "yes", "Yes"]:
             None
         else:
             print("Choose a valid option")
@@ -137,4 +139,3 @@ while True:
     else:
         print("something wrong, try again.")
         break
-    
